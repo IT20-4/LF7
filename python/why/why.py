640b3cd9ed911230e5082ea104a81ea324842bcb
Pe0nx - Notebook <83284383+Pe0nx@users.noreply.github.com>
2022-04-12 09:21:00 +0200

    wieso?

---
+++ python/why/why.py
@@ -0,0 +1,59 @@
+# Aufgabe 1
+print('Hello World')
+print()
+
+# Aufgabe 2
+print('BSZ7 Name Vorname')
+print()
+
+# Aufgabe 3
+for i in range(24):
+    print('Name')
+print()
+
+# Aufgabe 4
+x = 'Hallo Welt'
+print(x)
+x = 'Mein Erste'
+print(x)
+print()
+
+# Aufgabe 5
+# In fünf Jahren bin ich 29 Jahre alt.
+
+# Aufgabe 6
+x = 13
+print(x + 17)
+
+# Aufgabe 7
+# Hallo  Frau Ivanova! Sie sind 24 Jahre alt.
+
+# Aufgabe 8
+# false
+# false
+# true
+
+# Aufgabe 9
+print('''
+Hallo Thomas!
+Schön, dass du deine Programmierung-Skills in Python weiter-
+entwickelst! Bleib dran
+''')
+
+# Aufgabe 10
+# Am Ende des Inhalts der Print-Funktion werden entweder ein Leerzeichen oder 2 Bindestriche hinzugefügt.
+
+# Aufgabe 11
+print('7', end = '')
+print('12,5')
+
+# Aufgabe 12
+x = 150
+print(x>100)
+print (x<100)
+
+# Aufgabe 13
+x = 24
+y = 29
+z = 51
+
